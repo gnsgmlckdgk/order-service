0a6d3710a6723e7b573625b3a1bc0a009df4d4b7
Hwang <dohauzi@gmail.com>
2024-11-27 14:24:54 +0900

    Github 배포 파이프라인 추가 및 컨피그서버 설정 추가

--- src/main/java/com/polarbookshop/orderservice/config/ClientProperties.java
+++ src/main/java/com/polarbookshop/orderservice/config/ClientProperties.java
@@ -1,13 +1,22 @@
 package com.polarbookshop.orderservice.config;
 
+import ch.qos.logback.core.net.server.Client;
 import jakarta.validation.constraints.NotNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.net.URI;
 
+@Slf4j
 @ConfigurationProperties(prefix = "polar")  // 사용자 지정 속성 이름의 프리픽스
 public record ClientProperties(
 
         @NotNull
-        URI catalogServiceUri
-) {}
+        URI catalogServiceUri,
+
+        String greeting
+) {
+        public ClientProperties {
+                log.info("polar gretting = {}", greeting);
+        }
+}
