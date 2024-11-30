f6e8a0311a0b33b16f8bce8b947cba2fa2f933f3
dohauzi@gmail.com <dohauzi@gmail.com>
2024-11-30 18:05:27 +0900

    설정 변경

--- src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java
+++ src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java
@@ -1,15 +1,23 @@
 package com.polarbookshop.orderservice;
 
+import com.polarbookshop.orderservice.config.ClientProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@Slf4j
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
+		var ctx = SpringApplication.run(OrderServiceApplication.class, args);
+
+		// Bean이 제대로 로드되었는지 확인
+		var properties = ctx.getBean(ClientProperties.class);
+		log.info("Main: ClientProperties loaded = {}", properties);
+
 	}
 
 }
